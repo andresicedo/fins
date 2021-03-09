ba7cf8b5e4db0b99a1f5ba567734cf298c9ea086
Andres Icedo <andresicedo24@gmail.com>
2021-03-08 20:22:10 -0600

    updated purchasing button on dashboard

--- home.js
+++ home.js
@@ -90,7 +90,11 @@ let purchaseTemplate = `
                         <form class="form-container">
                             <h1>Vendor Options</h1>
 
-                            <button id="createVendorBtn" type="submit" class="createVendorBtn">Create Vendor</button>
+                            <button id="createVendorBtn" type="submit" class="createVendorBtn" onclick="createVendorForm(createVendorTemplate, '#createVendorContainer')">Create Vendor</button>
+                            <div id="createVendorContainer">
+                            </div>
+                            
+                            
                             <button type="submit" class="updateVendorBtn">Update Vendor</button>
                             <button type="submit" class="deleteVendorBtn">Delete Vendor</button>
                             <div id="createVendorDropdown" class="createVendorDropdown">
@@ -289,5 +293,113 @@ function closeVendorForm() {
 document.querySelector("#vendorForm").style.display = "none";
 }
 
-//On click of new vendor button
+//On click of create vendor button
+let createVendorForm = function(template, selector){
+    document.querySelector("#createVendorContainer").style.display = "block";
+    let node = document.querySelector(selector);
+    if(!node) return;
+    node.innerHTML = template;
+}
 
+let createVendorTemplate = `
+<form>
+  <div class="form-row">
+  <div class="form-group col-md-12">
+      <label for="vendorName">Vendor Name</label>
+      <input type="text" class="form-control" id="vendorName" placeholder="ABC Company">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="phoneNum">Contact Number</label>
+      <input type="tel" class="form-control" id="phoneNum" placeholder="000-000-0000">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Email</label>
+      <input type="email" class="form-control" id="inputEmail4">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Address 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-4">
+      <label for="inputCity">City</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputState">State</label>
+      <select name="state" id="inputState">
+  <option value="" selected="selected" disabled>State</option>
+  <option value="AL">AL</option>
+  <option value="AK">AK</option>
+  <option value="AZ">AZ</option>
+  <option value="AR">AR</option>
+  <option value="CA">CA</option>
+  <option value="CO">CO</option>
+  <option value="CT">CT</option>
+  <option value="DE">DE</option>
+  <option value="DC">DC</option>
+  <option value="FL">FL</option>
+  <option value="GA">GA</option>
+  <option value="HI">HI</option>
+  <option value="ID">ID</option>
+  <option value="IL">IL</option>
+  <option value="IN">IN</option>
+  <option value="IA">IA</option>
+  <option value="KS">KS</option>
+  <option value="KY">KY</option>
+  <option value="LA">LA</option>
+  <option value="ME">ME</option>
+  <option value="MD">MD</option>
+  <option value="MA">MA</option>
+  <option value="MI">MI</option>
+  <option value="MN">MN</option>
+  <option value="MS">MS</option>
+  <option value="MO">MO</option>
+  <option value="MT">MT</option>
+  <option value="NE">NE</option>
+  <option value="NV">NV</option>
+  <option value="NH">NH</option>
+  <option value="NJ">NJ</option>
+  <option value="NM">NM</option>
+  <option value="NY">NY</option>
+  <option value="NC">NC</option>
+  <option value="ND">ND</option>
+  <option value="OH">OH</option>
+  <option value="OK">OK</option>
+  <option value="OR">OR</option>
+  <option value="PA">PA</option>
+  <option value="RI">RI</option>
+  <option value="SC">SC</option>
+  <option value="SD">SD</option>
+  <option value="TN">TN</option>
+  <option value="TX">TX</option>
+  <option value="UT">UT</option>
+  <option value="VT">VT</option>
+  <option value="VA">VA</option>
+  <option value="WA">WA</option>
+  <option value="WV">WV</option>
+  <option value="WI">WI</option>
+  <option value="WY">WY</option>
+</select>
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputZip">Zip</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary" onclick="closeCreateVendorForm()">Create Vendor</button>
+</form>
+<br>
+<br>
+`;
+//On click of create vendor button
+function closeCreateVendorForm() {
+    document.querySelector("#createVendorContainer").style.display = "none";
+    }
+
+//On click of budget forecast button
